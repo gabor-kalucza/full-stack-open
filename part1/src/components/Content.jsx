83505477279046bc3a7eafda9859105812ecb833
Gabor Kalucza <gabor.kalucza@outlook.com>
2026-02-17 21:12:29 +0100

    feat: add exercise 1.3 solution

--- part1/src/components/Content.jsx
+++ part1/src/components/Content.jsx
@@ -1,18 +1,11 @@
 import { Part } from './Part'
 
-const Content = ({
-  part1,
-  part2,
-  part3,
-  exercises1,
-  exercises2,
-  exercises3,
-}) => {
+const Content = ({ part1, part2, part3 }) => {
   return (
     <>
-      <Part name={part1} numberOfExercise={exercises1} />
-      <Part name={part2} numberOfExercise={exercises2} />
-      <Part name={part3} numberOfExercise={exercises3} />
+      <Part name={part1.name} numberOfExercise={part1.exercises} />
+      <Part name={part2.name} numberOfExercise={part2.exercises} />
+      <Part name={part3.name} numberOfExercise={part3.exercises} />
     </>
   )
 }
