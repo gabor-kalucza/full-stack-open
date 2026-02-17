3910c0076960582fbdb107d407c46ceaa9330a89
Gabor Kalucza <gabor.kalucza@outlook.com>
2026-02-17 22:41:53 +0100

    feat: add exercise 1.11 solution

--- part1/unicafe/src/components/Statistics.jsx
+++ part1/unicafe/src/components/Statistics.jsx
@@ -13,14 +13,16 @@ const Statistics = ({
   }
 
   return (
-    <div>
-      <StatisticLine text='good' value={good} />
-      <StatisticLine text='neutral' value={neutral} />
-      <StatisticLine text='bad' value={bad} />
-      <StatisticLine text='all' value={total} />
-      <StatisticLine text='average' value={average} />
-      <StatisticLine text='positive' value={positivePercentage + ' %'} />
-    </div>
+    <table>
+      <tbody>
+        <StatisticLine text='good' value={good} />
+        <StatisticLine text='neutral' value={neutral} />
+        <StatisticLine text='bad' value={bad} />
+        <StatisticLine text='all' value={total} />
+        <StatisticLine text='average' value={average} />
+        <StatisticLine text='positive' value={positivePercentage + ' %'} />
+      </tbody>
+    </table>
   )
 }
 
