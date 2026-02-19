41f56721907090ed85aed580e3c51ac7c7983a3f
Gabor Kalucza <gabor.kalucza@outlook.com>
2026-02-19 13:43:29 +0100

    feat: add exercise 2.8 solution

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-1234567' },
+  ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleSubmit = (e) => {
     e.preventDefault()
     if (!persons.find((p) => p.name.toLowerCase() === newName.toLowerCase())) {
-      setPersons([...persons, { name: newName }])
+      setPersons([...persons, { name: newName, number: newNumber }])
     } else {
       alert(`${newName} is already added to phonebook`)
     }
@@ -22,13 +25,23 @@ const App = () => {
           <input value={newName} onChange={(e) => setNewName(e.target.value)} />
         </div>
         <div>
+          number:{' '}
+          <input
+            type='number'
+            value={newNumber}
+            onChange={(e) => setNewNumber(e.target.value)}
+          />
+        </div>
+        <div>
           <button type='submit'>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <div>
         {persons.map((p) => (
-          <p key={p.name}>{p.name}</p>
+          <p key={p.name}>
+            {p.name} {p.number}
+          </p>
         ))}
       </div>
     </div>
