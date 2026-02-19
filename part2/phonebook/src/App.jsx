0f113fee94e7a26a14b5fa1c312c3a435de0f456
Gabor Kalucza <gabor.kalucza@outlook.com>
2026-02-19 13:22:40 +0100

    feat: add exercise 2.6 solution

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+  const [newName, setNewName] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setPersons([...persons, { name: newName }])
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name:{' '}
+          <input value={newName} onChange={(e) => setNewName(e.target.value)} />
+        </div>
+        <div>
+          <button type='submit'>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map((p) => (
+          <p key={p.name}>{p.name}</p>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default App
