fb5556f5112ab800636ed7bc325e13059c5449a9
Gabor Kalucza <gabor.kalucza@outlook.com>
2026-02-19 09:51:35 +0100

    refactor: introduce inline expression for reduce

--- part2/exercises-2.1-2.5/src/components/Total.jsx
+++ part2/exercises-2.1-2.5/src/components/Total.jsx
@@ -1,9 +1,6 @@
 const Total = ({ course: { parts } }) => {
-  return (
-    <p style={{ fontWeight: 'bold' }}>
-      Total of {parts.reduce((sum, part) => sum + part.exercises, 0)} exercises
-    </p>
-  )
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+  return <p style={{ fontWeight: 'bold' }}>Total of {total} exercises</p>
 }
 
 export default Total
