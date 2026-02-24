9d8b45d522c7bd2c642bb9fd4bd23e6b8b98df26
Gabor Kalucza <gabor.kalucza@outlook.com>
2026-02-24 12:27:44 +0100

    feat: add exercise 3.2 solution

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,36 +1,21 @@
 const express = require('express')
+const persons = require('./persons')
 
 const PORT = 8080
 const app = express()
 
 app.use(express.json())
 
-let persons = [
-  {
-    id: '1',
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: '2',
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: '3',
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: '4',
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-]
-
 app.get('/api/persons', (req, res) => {
-  res.status(200)
   res.json(persons)
+})
+
+app.get('/info', (req, res) => {
+  const requestSentAt = new Date()
+
+  res.send(
+    `Phonebook has info for ${persons.length} people<br>${requestSentAt.toString()}`,
+  )
 })
 
 app.listen(PORT, () => {
