a3940a30bab208f602b06d13608c96a5c9a4f393
Gabor Kalucza <gabor.kalucza@outlook.com>
2026-02-24 12:11:06 +0100

    feat: add exercise 3.1 solution

---
+++ part3/phonebook-backend/index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+
+const PORT = 8080
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+  {
+    id: '1',
+    name: 'Arto Hellas',
+    number: '040-123456',
+  },
+  {
+    id: '2',
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+  },
+  {
+    id: '3',
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+  },
+  {
+    id: '4',
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+  },
+]
+
+app.get('/api/persons', (req, res) => {
+  res.status(200)
+  res.json(persons)
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
