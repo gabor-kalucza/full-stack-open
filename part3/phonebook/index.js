7a554c319fb563977d0ba428485376065bc6efd9
Gabor Kalucza <gabor.kalucza@outlook.com>
2026-02-25 14:26:06 +0100

    feat: add exercises 3.13-3.14 solutions

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -10,28 +10,9 @@ const app = express()
 
 app.use(express.static('dist'), express.json(), morgan('tiny'), cors())
 
-let persons = [
-  {
-    id: '1',
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: '2',
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: '3',
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: '4',
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-]
+let persons = Person.find({}).then((result) => {
+  persons = result
+})
 
 app.get('/info', (req, res) => {
   const requestSentAt = new Date()
@@ -59,7 +40,6 @@ app.get('/api/persons/:id', (req, res) => {
 })
 
 app.post('/api/persons', postLogger, (req, res) => {
-  const id = Math.random().toString(36).substring(2, 6)
   const { number, name } = req.body || {}
   const names = persons.map((p) => p.name.toLowerCase())
   const isNameExists = names.some((n) => n === name?.toLowerCase())
@@ -75,13 +55,13 @@ app.post('/api/persons', postLogger, (req, res) => {
   }
 
   const person = {
-    id,
     name,
     number,
   }
 
-  persons = persons.concat(person)
-  res.status(201).json(person)
+  Person.insertOne(person).then(() => {
+    res.status(201).json(person)
+  })
 })
 
 app.delete('/api/persons/:id', (req, res) => {
