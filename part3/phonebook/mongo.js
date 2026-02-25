09d5c71edc4fb55936e700878f37fae6a20e217b
Gabor Kalucza <gabor.kalucza@outlook.com>
2026-02-25 13:18:46 +0100

    fix: log only the name and number, instead of the whole person object

--- part3/phonebook/mongo.js
+++ part3/phonebook/mongo.js
@@ -31,7 +31,7 @@ if (process.argv.length >= 4) {
   console.log('phonebook:')
   Person.find({}).then((result) => {
     result.forEach((person) => {
-      console.log(person)
+      console.log(person.name, person.number)
     })
     mongoose.connection.close()
   })
