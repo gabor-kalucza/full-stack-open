36455cda91c616bfe82433d803ef819966a614b4
Gabor Kalucza <gabor.kalucza@outlook.com>
2026-02-26 10:34:46 +0100

    feat: add exercise 3.20 solution

--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -29,6 +29,12 @@ const personSchema = new mongoose.Schema({
   number: {
     type: String,
     required: true,
+    minLength: 8,
+    validate: {
+      validator: (v) => {
+        return /^\d{2,3}-\d+$/.test(v)
+      },
+    },
   },
 })
 
