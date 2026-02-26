4bdd0db2526ac0e2c6e81f4a2055e292f5ca8a4e
Gabor Kalucza <gabor.kalucza@outlook.com>
2026-02-26 15:51:26 +0100

    feat: add exercise 4.4 solution

---
+++ part4/tests/bloglist.test.js
@@ -0,0 +1,32 @@
+const listHelper = require('../utils/list_helper')
+const assert = require('node:assert')
+const { test, describe } = require('node:test')
+const blogs = require('./mock_data')
+
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0,
+    },
+  ]
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+
+  test('when list has many blogs, equals the sum of likes', () => {
+    const result = listHelper.totalLikes(blogs)
+    assert.strictEqual(result, 36)
+  })
+
+  test('when list has no blogs, equals zero', () => {
+    const result = listHelper.totalLikes([])
+    assert.strictEqual(result, 0)
+  })
+})
