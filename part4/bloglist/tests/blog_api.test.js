7164313c12ead1eba8e82a6c779cd871497d2f75
Gabor Kalucza <gabor.kalucza@outlook.com>
2026-02-27 11:32:53 +0100

    feat: add exercise 4.9 solution

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -45,6 +45,14 @@ describe('test blog api', () => {
     assert.strictEqual(response.body.length, initialBlogs.length)
   })
 
+  test('blogs have id property instead of _id', async () => {
+    const response = await api.get('/api/blogs')
+    const blog = response.body[0]
+
+    assert.ok(blog.id)
+    assert.strictEqual(blog._id, undefined)
+  })
+
   after(async () => {
     await mongoose.connection.close()
   })
