55768584af7394b5c20f04deed9a749e991c67cc
Gabor Kalucza <gabor.kalucza@outlook.com>
2026-03-16 13:22:17 +0100

    feat: add exercises 5.6-5.7 solutions

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,37 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  const toggleDetails = () => {
+    setShowDetails(!showDetails)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button onClick={toggleDetails}>
+        {showDetails ? 'Hide' : 'Show'} Details
+      </button>
+      <div>
+        {showDetails && (
+          <>
+            <div>{blog.url}</div>
+            <div>likes {blog.likes}</div>
+            <div>{blog.user && blog.user.name}</div>
+          </>
+        )}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
