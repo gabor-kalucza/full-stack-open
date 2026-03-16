a89b707168baebcb2bcd73f46bf2894df86accec
Gabor Kalucza <gabor.kalucza@outlook.com>
2026-03-16 13:51:44 +0100

    feat: add exercise 5.10 solution

--- part5/bloglist-frontend/src/components/BlogList.jsx
+++ part5/bloglist-frontend/src/components/BlogList.jsx
@@ -3,9 +3,11 @@ import Blog from './Blog'
 const BlogList = ({ blogs, notify }) => {
   return (
     <>
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} notify={notify} />
-      ))}
+      {blogs
+        .toSorted((a, b) => b.likes - a.likes)
+        .map((blog) => (
+          <Blog key={blog.id} blog={blog} notify={notify} />
+        ))}
     </>
   )
 }
